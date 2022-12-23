adadb69719d5e57b6913e8bb9bbcc6689168a33a
Marek Bartos <m.bartos@daolutions.com>
2022-12-23 16:36:52 +0100

    weird things

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
   import { browser, building, dev, version } from '$app/environment';
-  import Keycloak from "keycloak-js";
+  //import Keycloak from "keycloak-js";
   import { onMount } from 'svelte';
 
   export const ssr = false;
+
+  let test
 
    let instance = {
     url: "https://my-test.create.at/auth",
@@ -13,18 +15,26 @@
   
   onMount(async () => {
     if (browser) {
-      let k = Keycloak(instance);
-      console.log(k)
-      let initOptions = { onLoad: "login-required" };
-      k
-        .init(initOptions)
-        .then(function (authenticated) {
-          console.info("Authenticated");
-        })
-        .catch(function () {
-          k.login(); 
-          alert("failed to initialize");
-        });
+      await import('$lib/keycloak-js/dist/keycloak.js').then(Keycloak => {
+        //test = Keyloak.default
+          //let k = test(instance);
+        let k = window.Keycloak(instance);
+        //console.log(window)
+        //const k = new Keycloak().default();
+        //console.log(k)
+        let initOptions = { onLoad: "login-required" };
+        k
+          .init(initOptions)
+          .then(function (authenticated) {
+            console.info("Authenticated");
+          })
+          .catch(function () {
+            k.login(); 
+            alert("failed to initialize");
+          });
+      })
+      //let Keycloak = await import('./say.js');
+      
     }
 
 	});
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
+  // resolve: {
+  //   alias: [
+  //     {find: /^keycloak-js\//, replacement: '/node_modules/keycloak-js/'}
+  //   ]
+  // },
 	plugins: [sveltekit()]
 };
 
